74ac3749bf422d5e8223c64990b54e2a900a2172
shenyong <shenyong812@hotmail.com>
2016-06-08 18:26:27 +0800

    event事件

---
+++ app/src/main/jni/JniEvent.h
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 2016/6/7.
+//
+
+#ifndef JNIMULTITHREAD_JNIEVENT_H_H
+#define JNIMULTITHREAD_JNIEVENT_H_H
+
+enum Event
+{
+    UE_INFO_MODIFY = 4,
+    UE_GET_LATLNG
+};
+
+#endif //JNIMULTITHREAD_JNIEVENT_H_H
