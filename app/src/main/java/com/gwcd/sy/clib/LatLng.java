44b55305afaac3bcc3e38ee0e32a3a247deff390
shenyong <shenyong812@hotmail.com>
2016-06-08 18:20:32 +0800

    添加LatLng

---
+++ app/src/main/java/com/gwcd/sy/clib/LatLng.java
@@ -0,0 +1,19 @@
+/**
+ * 项目名称：JniMultiThread
+ * 创建日期：2016年06月07日
+ * Copyright 2016 GALAXYWIND Network Systems Co.,Ltd.All rights reserved.
+ */
+package com.gwcd.sy.clib;
+
+/**
+ * 类描述：<br>
+ * 创建者：shenyong<br>
+ * 创建时间：2016/6/7<br>
+ * 修改记录：<br>
+ */
+public class LatLng {
+
+    public double latitude;
+
+    public double longitude;
+}
